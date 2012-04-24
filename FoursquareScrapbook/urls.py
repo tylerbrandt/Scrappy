f7125ed1da6a74f685bc5fa47f7ca2e7a53eb349
Tyler Brandt <nu.ty08@gmail.com>
2012-04-24 08:00:12 -0700

    Enabled admin docs (requires pip install docutils)

--- FoursquareScrapbook/urls.py
+++ FoursquareScrapbook/urls.py
@@ -10,7 +10,7 @@ urlpatterns = patterns('',
     # url(r'^FoursquareScrapbook/', include('FoursquareScrapbook.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
