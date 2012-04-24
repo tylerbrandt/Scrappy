131f5942e5d0b712bd4dce80a7d13b0d11ed53fb
Tyler Brandt <nu.ty08@gmail.com>
2012-04-24 09:30:39 -0700

    Enable serving static files from /media

--- FoursquareScrapbook/urls.py
+++ FoursquareScrapbook/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -8,6 +9,9 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'FoursquareScrapbook.views.home', name='home'),
     # url(r'^FoursquareScrapbook/', include('FoursquareScrapbook.foo.urls')),
+
+    # static files serve
+    url(r'^media/(?P<path>.*)', 'django.views.static.serve', { 'document_root': settings.MEDIA_ROOT }),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
